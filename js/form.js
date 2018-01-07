4de6650f95f255b20985c06cd6b655d9e31d37de
nyree <nyree.mereus@turner.com>
2018-01-07 14:30:59 -0500

    Adding new login, registration, and checkout forms

---
+++ js/form.js
@@ -0,0 +1,156 @@
+
+            var currentTab = 0; // Current tab is set to be the first tab (0)
+            showTab(currentTab); // Display the crurrent tab
+            
+            function showTab(n) {
+              // This function will display the specified tab of the form...
+              var x = document.getElementsByClassName("tab");
+              x[n].style.display = "block";
+              //... and fix the Previous/Next buttons:
+              if (n == 0) {
+                document.getElementById("prevBtn").style.display = "none";
+              } else {
+                document.getElementById("prevBtn").style.display = "inline";
+              }
+              if (n == (x.length - 1)) {
+                document.getElementById("nextBtn").innerHTML = "Submit";
+              } else {
+                document.getElementById("nextBtn").innerHTML = "Next";
+              }
+              //... and run a function that will display the correct step indicator:
+              fixStepIndicator(n)
+            }
+            
+            function nextPrev(n) {
+              // This function will figure out which tab to display
+              var x = document.getElementsByClassName("tab");
+              // Exit the function if any field in the current tab is invalid:
+              if (n == 1 && !validateForm()) return false;
+              // Hide the current tab:
+              x[currentTab].style.display = "none";
+              // Increase or decrease the current tab by 1:
+              currentTab = currentTab + n;
+              // if you have reached the end of the form...
+              if (currentTab >= x.length) {
+                // ... the form gets submitted:
+                document.getElementById("regForm").submit();
+                return false;
+              }
+              // Otherwise, display the correct tab:
+              showTab(currentTab);
+            }
+            
+            function validateForm() {
+              // This function deals with validation of the form fields
+              var x, y, i, valid = true;
+              x = document.getElementsByClassName("tab");
+              y = x[currentTab].getElementsByTagName("input");
+              // A loop that checks every input field in the current tab:
+              for (i = 0; i < y.length; i++) {
+                // If a field is empty...
+                if (y[i].value == "") {
+                  // add an "invalid" class to the field:
+                  y[i].className += " invalid";
+                  // and set the current valid status to false
+                  valid = false;
+                }
+              }
+              // If the valid status is true, mark the step as finished and valid:
+              if (valid) {
+                document.getElementsByClassName("step")[currentTab].className += " finish";
+              }
+              return valid; // return the valid status
+            }
+            
+            function fixStepIndicator(n) {
+              // This function removes the "active" class of all steps...
+              var i, x = document.getElementsByClassName("step");
+              for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace(" active", "");
+              }
+              //... and adds the "active" class on the current step:
+              x[n].className += " active";
+            }
+
+            function is_int(value){ 
+                if ((parseFloat(value) == parseInt(value)) && !isNaN(value)) {
+                  return true;
+                } else { 
+                  return false;
+                } 
+              }
+
+            //for the zipcode, hide all the address fields 
+            $(".street, .state, .city, .zip-error").hide();
+
+            //<![CDATA[
+	$(function() {
+		// IMPORTANT: Fill in your client key
+		var clientKey = "js-XsVw2lWd6nTU4JzklvrnhH6kKaO6zsOrQVUxVXT0mP7VrSkWqpASYZZqhFOIn7Mb";
+		
+		var cache = {};
+		var container = $(".zip-code-section");
+		var errorDiv = container.find("div.zip-error");
+		
+		/** Handle successful response */
+		function handleResp(data)
+		{
+			// Check for error
+			if (data.error_msg)
+				errorDiv.text(data.error_msg);
+			else if ("city" in data)
+			{
+                $(".street, .state, .city").slideDown(); /* Show the fields */
+				// Set city and state
+				container.find("input[name='city']").val(data.city);
+				container.find("input[name='state']").val(data.state);
+			}
+		}
+		
+		// Set up event handlers
+		container.find("input[name='zipcode']").on("keyup change", function() {
+			// Get zip code
+			var zipcode = $(this).val().substring(0, 5);
+			if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode))
+			{
+				// Clear error
+				errorDiv.empty();
+				
+				// Check cache
+				if (zipcode in cache)
+				{
+					handleResp(cache[zipcode]);
+				}
+				else
+				{
+					// Build url
+					var url = "https://www.zipcodeapi.com/rest/"+clientKey+"/info.json/" + zipcode + "/radians";
+					
+					// Make AJAX request
+					$.ajax({
+						"url": url,
+						"dataType": "json"
+					}).done(function(data) {
+						handleResp(data);
+						
+						// Store in cache
+						cache[zipcode] = data;
+					}).fail(function(data) {
+						if (data.responseText && (json = $.parseJSON(data.responseText)))
+						{
+							// Store in cache
+							cache[zipcode] = json;
+							
+							// Check for error
+                            if (json.error_msg)
+                             $(".zip-error").show(); /* Ruh row */
+								errorDiv.text(json.error_msg);
+						}
+						else
+							errorDiv.text('Request failed.');
+					});
+				}
+			}
+		}).trigger("change");
+	});
+//]]>
